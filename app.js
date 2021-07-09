60d6a50f0e35571aef3e91ae75e4adce64049377
AMIT KUMAR <amitk.ug19.cs@nitp.ac.in>
2021-07-09 14:01:54 +0530

    Second Commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/clientDB" , {useNewUrlParser:true});
+mongoose.connect("mongodb+srv://admin-amit:Test-123@cluster0.kxfkn.mongodb.net/blogDB" , {useNewUrlParser:true});
 
 var springedge = require('springedge')
 
